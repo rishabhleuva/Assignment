0347bf4ca26b8332c6ca09214a28088f68d5a949
RISHABH <leuvarishabh@gmail.com>
2025-08-13 23:59:12 +0530

    Add files via upload

---
+++ MODULE 6/q4.java
@@ -0,0 +1,16 @@
+package assignment;
+
+import java.util.Scanner;
+
+public class q4 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        if(n % 2 == 0) System.out.println("Even");
+        else System.out.println("Odd");
+
+	}
+
+}

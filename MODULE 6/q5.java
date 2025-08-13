0253eb978d22eb481e245e56972ae413838587de
RISHABH <leuvarishabh@gmail.com>
2025-08-14 00:18:49 +0530

    Add files via upload

---
+++ MODULE 6/q5.java
@@ -0,0 +1,17 @@
+package assignment;
+import java.util.Scanner;
+
+public class q5 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	    Scanner sc = new Scanner(System.in);
+        int choice = sc.nextInt();
+        switch(choice) {
+            case 1: System.out.println("Option 1"); break;
+            case 2: System.out.println("Option 2"); break;
+            default: System.out.println("Invalid");
+
+	}
+
+}

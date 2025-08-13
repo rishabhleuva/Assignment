226a2c4f031d4a1813c75a819c27bc7a2194eecb
RISHABH <leuvarishabh@gmail.com>
2025-08-13 23:53:55 +0530

    Add files via upload

---
+++ MODULE 6/q2.java
@@ -0,0 +1,26 @@
+package assignment;
+
+public class q2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		byte b = 100;
+        short s = 20000;
+        int i = 100000;
+        long l = 10000000000L;
+        float f = 3.14f;
+        double d = 3.1415926535;
+        char c = 'A';
+        boolean bool = true;
+
+        System.out.println("Byte value: " + b);
+        System.out.println("Short value: " + s);
+        System.out.println("Int value: " + i);
+        System.out.println("Long value: " + l);
+        System.out.println("Float value: " + f);
+        System.out.println("Double value: " + d);
+        System.out.println("Char value: " + c);
+        System.out.println("Boolean value: " + bool);
+	}
+
+}
